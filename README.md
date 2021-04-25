6428fbb9372b5aba5e2af3146803c9549626d93c
jessev14 <jessevo321@gmail.com>
2021-04-25 18:00:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Darkness Animation Duration
+FoundryVTT module that allows users to set a custom duration for the scene darkness animation.
+
+# Usage
+A custom default animation duration (in milliseconds) can be set in the module settings. The Foundry default is 10000msec (10sec).
+Clicking the Transition to Day/Night buttons in the Light toolbar will change the scene darkness to 0/1.0 with the set duration.
+
+A custom button is added to the Light toolbar that lets users input a target light darkness level and duration. The button can be removed in the module settings.
+
+A custom scene darkness transition can also be triggered programmatically:
+```js
+game.modules.get("darknessAnimationDuration").api.customDarknessDuration(darknessLevel, duration);
+```
+
+# Technical Notes
+A socket is used to initate the animation on other clients. The `LightingLayer#animateDarkness` core function is used to initiate animation on current client).
+After animation, the scene darkness is manually updated.
+
+I submitted [this issue](https://gitlab.com/foundrynet/foundryvtt/-/issues/4715) to the core Foudndry repo so I fully expect this module to become obsolete at some point.
+I've just been using this for my own purposes and thought I might as well clean it up and share.
+
+If you run into any issues, please reach out: `enso#0361`
---
+++ scripts/darknessAnimationDuration.js
@@ -0,0 +1,109 @@
+Hooks.once("init", () => {
+    // register settings
+    game.settings.register("darknessAnimationDuration", "customTool", {
+        name: game.i18n.localize("darknessAnimationDuration.settings.customTool.name"),
+        hint: "",
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+    });
+
+    game.settings.register("darknessAnimationDuration", "defaultDuration", {
+        name: game.i18n.localize("darknessAnimationDuration.settings.defaultDuration.name"),
+        hint: game.i18n.localize("darknessAnimationDuration.settings.defaultDuration.hint"),
+        scope: "world",
+        config: true,
+        type: Number,
+        default: 10000
+    });
+});
+
+Hooks.on("getSceneControlButtons", (controls) => {
+    if (!game.user.isGM) return;
+    const defaultDuration = game.settings.get("darknessAnimationDuration", "defaultDuration")
+    const bar = controls.find(c => c.name === "lighting");
+    bar.tools.find(t => t.name === "day").onClick = () => customDuration(0, defaultDuration);
+    bar.tools.find(t => t.name === "night").onClick = () => customDuration(1.0, defaultDuration);
+    if (game.settings.get("darknessAnimationDuration", "customTool")) {
+        bar.tools.splice(3, 0, {
+            name: "customTransition",
+            title: "Custom Transition",
+            icon: "far fa-sun",
+            onClick: () => darknessAnimationDialog(),
+            button: true
+        });
+    }
+});
+
+Hooks.once("ready", () => {
+    // socket for animateDarkness duration
+    game.socket.on(`module.darknessAnimationDuration`, (data) => {
+        canvas.lighting.animateDarkness(data.target, { duration: data.duration });
+    });
+
+    //add function to global scope to allow macros to use
+    const moduleAPI = game.modules.get("darknessAnimationDuration").api = {};
+    moduleAPI.customDarknessDuration = customDuration;
+});
+
+async function darknessAnimationDialog() {
+    const buttonPosition = $(document).find("li.control-tool[data-tool='customTransition']").offset();
+    console.log(buttonPosition)
+    const options = {
+        width: 300,
+        top: buttonPosition.top,
+        left: buttonPosition.left + 50
+    }
+    const content = await renderTemplate("modules/darknessAnimationDuration/templates/customAnimationDialog.hbs", { placeholder: game.settings.get("darknessAnimationDuration", "defaultDuration") });
+    new Dialog({
+        title: "Custom Scene Darkness Animation",
+        content,
+        buttons: {
+            confirm: {
+                label: game.i18n.localize("darknessAnimationDuration.dialog.confirm"),
+                callback: async (html) => {
+                    const target = parseInt(html.find("#target").val() || html.find("#target").attr("placeholder"));
+                    const duration = parseInt(html.find("#duration").val() || html.find("#duration").attr("placeholder"));
+                    if (game.scenes.viewed.data.darkness === target) return;
+                    await customDuration(target, duration);
+                }
+            },
+            cancel: {
+                label: game.i18n.localize("darknessAnimationDuration.dialog.cancel"),
+                callback: () => { }
+            }
+        },
+        default: "confirm"
+    }, options).render(true);
+}
+
+/**
+   * Animate a smooth transition of the darkness overlay to a target value.
+   * Only begin animating if another animation is not already in progress.
+   * If called without second argument, duration will be set to default in module settings.
+   * If called without any arguments, darkness level will be set to 1.
+   * @param {number} target     The target darkness level between 0 and 1.
+   * @param {number} duration   The desired animation time in milliseconds.
+   */
+async function customDuration(target = 1.0, duration) {
+    if (canvas.lighting._animating) return;
+    if (game.scenes.viewed.data.darkness === target) return;
+    if (!duration) duration = game.settings.get("darknessAnimationDuration", "defaultDuration");
+
+    // emit socket to initiate custom darkness animation on other clients
+    game.socket.emit("module.darknessAnimationDuration", { target, duration });
+    // initiate custom darkness animation on current client
+    canvas.lighting.animateDarkness(target, { duration });
+    // wait for animation to complete
+    await delay();
+    // actually update scene darkness level
+    await game.scenes.viewed.update({ darkness: target });
+
+    //ui.notifications.info("Scene darkness updated.");
+
+
+    async function delay() {
+        await new Promise(resolve => setTimeout(resolve, duration + 500));
+    }
+}
